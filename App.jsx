f29d88973a4d69b3397c7ae8ca60eb5ab56ef459
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-15 20:45:45 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,52 @@
+import React, { useCallback } from 'react';
+import { StyleSheet,
+        SafeAreaView,
+        Platform,
+        StatusBar } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts,
+        Inter_600SemiBold,
+        Inter_400Regular } from '@expo-google-fonts/inter';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Routes } from './src/routes';
+
+
+export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Inter_600SemiBold,
+    Inter_400Regular
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView
+        style={styles.safe}
+        onLayout={onLayoutRootView}>
+        <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'#18181b'}
+        translucent={true} />
+        <NavigationContainer>
+            <Routes />
+        </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
