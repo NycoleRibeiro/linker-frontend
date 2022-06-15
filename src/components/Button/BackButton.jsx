2dfa8a90e46c5d8773ca15304ddddd8b99cccc49
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-15 20:47:32 -0300

    Create BackButton.jsx

---
+++ src/components/Button/BackButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableHighlight } from 'react-native';
+import { StyleSheet } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons';
+
+function BackButton(props) {
+    return (
+        <TouchableHighlight
+            style={css.backButton}
+            onPress={props.voltar} >
+            <MaterialIcons
+                name="keyboard-arrow-left"
+                size={50}
+                color="white" />
+        </TouchableHighlight>
+
+    );
+  }
+
+  export default BackButton;
+
+  const css = StyleSheet.create({
+    backButton: {
+        position: 'absolute',
+        top: '3%',
+    },
+  });
