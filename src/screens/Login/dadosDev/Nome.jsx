6155b4516cb4d093077f10d752cdf7b597f8fab1
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-15 21:59:21 -0300

    Update Nome.jsx

--- src/screens/Login/dadosDev/Nome.jsx
+++ src/screens/Login/dadosDev/Nome.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View,
     Text,
+    TextInput,
     ScrollView,}
 from 'react-native';
+import { StyleSheet } from "react-native";
 
 import { css } from './Css.js';
 import ContinuarButton from '../../../components/Button/ContinuarButton';
 import ProgressBar from '../../../components/Input/ProgressBar';
-import InputText from '../../../components/Input/InputText';
 
 
 function Nome({navigation}) {
+    const [nome, setNome] = React.useState("");
+
+    function handleNome(text) {
+        console.log("Nome:",nome);
+        navigation.navigate('Numero')
+    }
+
     return (
         <View
         style={css.container}
@@ -21,15 +29,43 @@ function Nome({navigation}) {
                     Meu {'\n'}
                     nome é
                 </Text>
-                <InputText
+
+                <TextInput
+                    style={style.input}
                     placeholder="Nome e Sobrenome"
-                    margintop="30%"/>
+                    placeholderTextColor="#A1A1AA"
+                    selectionColor="#f4f4f5"
+                    keyboardType="default"
+                    autoCapitalize="words"
+                    maxLength={30}
+                    onChangeText={(text) => {
+                        setNome(text);
+                    }}
+                />
             </ScrollView>
             <ContinuarButton
-                onPress={() => navigation.navigate('Numero')}
+                onPress={handleNome}
                 />
         </View>
     );
 }
 
 export default Nome;
+
+const style = StyleSheet.create({
+    input: {
+        width: '80%',
+        borderWidth: 1,
+        paddingLeft: 5,
+        marginRight: 40,
+        marginLeft: 40,
+        marginTop: "30%",
+        borderBottomColor: '#FD2B7A',
+        borderTopColor: '#18181b',
+        borderLeftColor: '#18181b',
+        borderRightColor: '#18181b',
+        fontFamily: 'Inter_400Regular',
+        fontSize: 20,
+        color: '#f4f4f5',
+      },
+});
