ad5fd459b85ec10907df292e06a830bca6b71194
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-16 19:51:22 -0300

    Update Local.jsx

--- src/screens/Login/dadosEmpresa/Local.jsx
+++ src/screens/Login/dadosEmpresa/Local.jsx
@@ -1,22 +1,27 @@
 
 import React from 'react';
 import { View,
-    Text, }
+        Text,
+        ScrollView, }
 from 'react-native';
 
 import { css } from './Css.js';
 import ContinuarButton from '../../../components/Button/ContinuarButton';
 import ProgressBar from '../../../components/Input/ProgressBar';
+import BackButton from '../../../components/Button/BackButton';
 
 function Local({navigation}) {
 
         return (
             <View style={css.container}>
                 <ProgressBar percent="100%"/>
-                <Text style={css.h1}>
-                    Local {'\n'}
-                    da Empresa
-                </Text>
+                <ScrollView>
+                    <BackButton voltar={() => navigation.goBack()}/>
+                    <Text style={css.h1}>
+                        Local {'\n'}
+                        da Empresa
+                    </Text>
+                </ScrollView>
                 <ContinuarButton
                     name="CONCLUIR"
                     onPress={() => navigation.reset({
