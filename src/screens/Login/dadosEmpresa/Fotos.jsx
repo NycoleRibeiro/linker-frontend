ea6883a679f6fb6fb6413826c40b1b919d12af0e
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-16 19:51:14 -0300

    Update Fotos.jsx

--- src/screens/Login/dadosEmpresa/Fotos.jsx
+++ src/screens/Login/dadosEmpresa/Fotos.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { View,
-    Text, }
+        Text,
+        ScrollView, }
 from 'react-native';
 
 import { css } from './Css.js';
 import ContinuarButton from '../../../components/Button/ContinuarButton';
 import ProgressBar from '../../../components/Input/ProgressBar';
+import BackButton from '../../../components/Button/BackButton';
 
 function Fotos({navigation}) {
 
         return (
             <View style={css.container}>
                 <ProgressBar percent="60%"/>
-                <Text style={css.h1}>
-                    Adicionar {'\n'}
-                    Fotos
-                </Text>
+                <ScrollView>
+                    <BackButton voltar={() => navigation.goBack()}/>
+                    <Text style={css.h1}>
+                        Adicionar {'\n'}
+                        Fotos
+                    </Text>
+                </ScrollView>
                 <ContinuarButton
                     onPress={() => navigation.navigate('Sobre')}
                 />
