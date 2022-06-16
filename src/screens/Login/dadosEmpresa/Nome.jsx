22b664c120f6fbd5da4c8f30b4c6dc074e2be5f6
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-16 17:35:34 -0300

    Update Nome.jsx

--- src/screens/Login/dadosEmpresa/Nome.jsx
+++ src/screens/Login/dadosEmpresa/Nome.jsx
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View,
-    Text,}
+    Text,
+    ScrollView,
+    TextInput,}
 from 'react-native';
+import { StyleSheet } from "react-native";
 
 import { css } from './Css.js';
 import ContinuarButton from '../../../components/Button/ContinuarButton';
 import ProgressBar from '../../../components/Input/ProgressBar';
 
-function Nome({navigation}) {
 
-        return (
-            <View style={css.container}>
-                <ProgressBar percent="20%"/>
+function Nome({navigation}) {
+    const [nome, setNome] = React.useState("");
+
+    function handleNome(text) {
+        console.log("Nome:",nome);
+        navigation.navigate('Numero')
+    }
+
+    return (
+        <View
+        style={css.container}
+        enabled={true}>
+            <ProgressBar percent="20%"/>
+            <ScrollView>
                 <Text style={css.h1}>
                     Nome {'\n'}
                     da Empresa
                 </Text>
-                <ContinuarButton
-                    onPress={() => navigation.navigate('Numero')}
+
+                <TextInput
+                    style={style.input}
+                    placeholder="Nome e Sobrenome"
+                    placeholderTextColor="#A1A1AA"
+                    selectionColor="#f4f4f5"
+                    keyboardType="default"
+                    autoCapitalize="words"
+                    maxLength={30}
+                    onChangeText={(text) => {
+                        setNome(text);
+                    }}
                 />
-            </View>
-        );
-    }
+            </ScrollView>
+            <ContinuarButton
+                onPress={handleNome}
+                />
+        </View>
+    );
+}
 
 export default Nome;
+
+const style = StyleSheet.create({
+    input: {
+        width: '80%',
+        borderWidth: 1,
+        paddingLeft: 5,
+        marginRight: 40,
+        marginLeft: 40,
+        marginTop: "30%",
+        borderBottomColor: '#FD2B7A',
+        borderTopColor: '#18181b',
+        borderLeftColor: '#18181b',
+        borderRightColor: '#18181b',
+        fontFamily: 'Inter_400Regular',
+        fontSize: 20,
+        color: '#f4f4f5',
+      },
+});
