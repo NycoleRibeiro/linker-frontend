4c85c634f6e2b77ae603662002a5abab36def527
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-16 19:51:17 -0300

    Update Numero.jsx

--- src/screens/Login/dadosEmpresa/Numero.jsx
+++ src/screens/Login/dadosEmpresa/Numero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View,
-    Text, }
+        Text,
+        ScrollView, }
 from 'react-native';
 
 import { css } from './Css.js';
@@ -22,54 +23,56 @@ function Numero({navigation}) {
     return (
         <View style={css.container}>
             <ProgressBar percent="40%"/>
-            <BackButton voltar={() => navigation.goBack()}/>
-            <Text style={css.h1}>
-                Número {'\n'}
-                da Empresa
-            </Text>
-            <PhoneInput
-            defaultCode="BR"
-            layout="second"
-            placeholder="DDD + Número"
-            withDarkTheme
-            onChangeText={(text) => {
-                setValue(text);
-            }}
-            onChangeFormattedText={(text) => {
-                setFormattedValue(text);
-            }}
-            containerStyle={{
-                backgroundColor: '#18181b',
-                marginLeft: 40,
-                marginTop: '30%',
-                height: 50,
-                width: '80%',
-            }}
-            textContainerStyle={{
-                backgroundColor: '#18181b',
-                paddingLeft: 10,
-                marginLeft: 10,
-                borderWidth: 1,
-                borderBottomColor: '#FD2B7A',
-                borderTopColor: '#18181b',
-                borderLeftColor: '#18181b',
-                borderRightColor: '#18181b',
-            }}
-            textInputStyle={{
-                fontFamily: 'Inter_400Regular',
-                color: '#f4f4f5',
-            }}
-            codeTextStyle={{
-                color: '#f4f4f5',
-            }}
-            textInputProps={{
-                placeholderTextColor: '#A1A1AA',
-            }}
-            countryPickerButtonStyle={{
-                backgroundColor: '#1C1C20',
-                borderRadius: 20,
-            }}
-            />
+            <ScrollView>
+                <BackButton voltar={() => navigation.goBack()}/>
+                <Text style={css.h1}>
+                    Número {'\n'}
+                    da Empresa
+                </Text>
+                <PhoneInput
+                defaultCode="BR"
+                layout="second"
+                placeholder="DDD + Número"
+                withDarkTheme
+                onChangeText={(text) => {
+                    setValue(text);
+                }}
+                onChangeFormattedText={(text) => {
+                    setFormattedValue(text);
+                }}
+                containerStyle={{
+                    backgroundColor: '#18181b',
+                    marginLeft: 40,
+                    marginTop: '30%',
+                    height: 50,
+                    width: '80%',
+                }}
+                textContainerStyle={{
+                    backgroundColor: '#18181b',
+                    paddingLeft: 10,
+                    marginLeft: 10,
+                    borderWidth: 1,
+                    borderBottomColor: '#FD2B7A',
+                    borderTopColor: '#18181b',
+                    borderLeftColor: '#18181b',
+                    borderRightColor: '#18181b',
+                }}
+                textInputStyle={{
+                    fontFamily: 'Inter_400Regular',
+                    color: '#f4f4f5',
+                }}
+                codeTextStyle={{
+                    color: '#f4f4f5',
+                }}
+                textInputProps={{
+                    placeholderTextColor: '#A1A1AA',
+                }}
+                countryPickerButtonStyle={{
+                    backgroundColor: '#1C1C20',
+                    borderRadius: 20,
+                }}
+                />
+            </ScrollView>
             <ContinuarButton
                 onPress={handleNumber}
             />
