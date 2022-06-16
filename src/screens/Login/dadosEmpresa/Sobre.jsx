c523a3355ae1fecb936a761816bec90f5d3dae8d
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-16 19:51:19 -0300

    Update Sobre.jsx

--- src/screens/Login/dadosEmpresa/Sobre.jsx
+++ src/screens/Login/dadosEmpresa/Sobre.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { View,
-    Text, }
+        Text,
+        ScrollView, }
 from 'react-native';
 
 import { css } from './Css.js';
 import ContinuarButton from '../../../components/Button/ContinuarButton';
 import ProgressBar from '../../../components/Input/ProgressBar';
+import BackButton from '../../../components/Button/BackButton';
 
 function Sobre({navigation}) {
 
         return (
             <View style={css.container}>
                 <ProgressBar percent="80%"/>
-                <Text style={css.h1}>
-                    Sobre {'\n'}
-                    a Empresa
-                </Text>
+                <ScrollView>
+                    <BackButton voltar={() => navigation.goBack()}/>
+                    <Text style={css.h1}>
+                        Sobre {'\n'}
+                        a Empresa
+                    </Text>
+                </ScrollView>
                 <ContinuarButton
                     onPress={() => navigation.navigate('Local')}
                 />
