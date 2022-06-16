5f53e31d1454ac984493a9bc8088f49494dde589
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-15 21:57:30 -0300

    Update Numero.jsx

--- src/screens/Login/dadosDev/Numero.jsx
+++ src/screens/Login/dadosDev/Numero.jsx
@@ -7,10 +7,17 @@ import { css } from './Css.js';
 import ContinuarButton from '../../../components/Button/ContinuarButton';
 import ProgressBar from '../../../components/Input/ProgressBar';
 import BackButton from '../../../components/Button/BackButton';
-import PhoneNumber from '../../../components/Input/PhoneNumber';
+import PhoneInput from "react-native-phone-number-input";
 
 
 function Numero({navigation}) {
+    const [value, setValue] = useState("");
+    const [formattedValue, setFormattedValue] = useState("");
+
+    function handleNumber(text) {
+        console.log("Celular:", formattedValue);
+        navigation.navigate('Fotos')
+    }
 
     return (
         <View style={css.container}>
@@ -20,9 +27,51 @@ function Numero({navigation}) {
                 Meu {'\n'}
                 Número é
             </Text>
-            <PhoneNumber/>
+            <PhoneInput
+            defaultCode="BR"
+            layout="second"
+            placeholder="DDD + Número"
+            withDarkTheme
+            onChangeText={(text) => {
+                setValue(text);
+            }}
+            onChangeFormattedText={(text) => {
+                setFormattedValue(text);
+            }}
+            containerStyle={{
+                backgroundColor: '#18181b',
+                marginLeft: 40,
+                marginTop: '30%',
+                height: 50,
+                width: '80%',
+            }}
+            textContainerStyle={{
+                backgroundColor: '#18181b',
+                paddingLeft: 10,
+                marginLeft: 10,
+                borderWidth: 1,
+                borderBottomColor: '#FD2B7A',
+                borderTopColor: '#18181b',
+                borderLeftColor: '#18181b',
+                borderRightColor: '#18181b',
+            }}
+            textInputStyle={{
+                fontFamily: 'Inter_400Regular',
+                color: '#f4f4f5',
+            }}
+            codeTextStyle={{
+                color: '#f4f4f5',
+            }}
+            textInputProps={{
+                placeholderTextColor: '#A1A1AA',
+            }}
+            countryPickerButtonStyle={{
+                backgroundColor: '#1C1C20',
+                borderRadius: 20,
+            }}
+            />
             <ContinuarButton
-                onPress={() => navigation.navigate('Fotos')}
+                onPress={handleNumber}
             />
         </View>
     );
