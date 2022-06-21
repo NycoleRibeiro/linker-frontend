0fb56d0812182d166fe5a3245ffd31128b44d06e
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-21 19:27:14 -0300

    feat

--- src/screens/Login/dadosDev/Fotos.jsx
+++ src/screens/Login/dadosDev/Fotos.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View,
         Text,
         ScrollView,
         Image,
-        Button,
-        TouchableHighlight, }
+        TouchableHighlight,}
 from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { StyleSheet } from "react-native";
@@ -13,45 +12,40 @@ import { css } from './Css.js';
 import ContinuarButton from '../../../components/Button/ContinuarButton';
 import ProgressBar from '../../../components/Input/ProgressBar';
 import BackButton from '../../../components/Button/BackButton';
+import ErrorMessage from '../../../components/ErrorMessage';
 
 
 function Fotos({navigation}) {
-    const [image, setImage] = useState(null);
-    const [image2, setImage2] = useState(null);
+    const [images, setImages] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(false);
 
     const pickImage = async () => {
-        // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             allowsMultipleSelection: false,
             aspect: [3, 5],
             quality: 1,
         });
 
-        console.log(result);
+        console.log("Imagem cadastrada");
 
         if (!result.cancelled) {
-            setImage(result.uri);
+            // Se não cancelado, adiciona a imagem no array
+            setImages(prevState => [...prevState, result.uri]);
         }
     };
 
-    const pickImage2 = async () => {
-        // No permissions request is necessary for launching the image library
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            allowsMultipleSelection: false,
-            aspect: [3, 5],
-            quality: 1,
-        });
-
-        console.log(result);
-
-        if (!result.cancelled) {
-            setImage2(result.uri);
+    function handleImages() {
+        if (images.length == 0) {
+            // Se não tiver nenhuma imagem cadastrada, mostra o ErrorMessage
+            setErrorMessage(true);
+        } else {
+            // Retorna o array de imagens cadastradas e vai para a próxima tela
+            console.log(images);
+            navigation.navigate('Sobre');
         }
-    };
+    }
 
     return (
         <View style={css.container}>
@@ -64,10 +58,13 @@ function Fotos({navigation}) {
                 </Text>
                 <View style={styles.container}>
 
+                    {/* Container da Foto 1 */}
                     <View style={styles.fotoContainer}>
-                        {image && <Image source={{ uri: image }} style={styles.foto} />}
+                        {/* Mostra a Foto 1 se existir */}
+                        {images[0] && <Image source={{ uri: images[0] }} style={styles.foto} />}
 
-                        {!image &&
+                        {/* Botão para adicionar a Foto 1 */}
+                        {!images[0] &&
                         <TouchableHighlight
                         style={styles.buttonAdd}
                         onPress={pickImage}>
@@ -76,10 +73,15 @@ function Fotos({navigation}) {
                             style={styles.buttonImage}/>
                         </TouchableHighlight>}
 
-                        {image &&
+                        {/* Botão para remover a Foto 1 */}
+                        {images[0] &&
                         <TouchableHighlight
                         style={styles.buttonDel}
-                        onPress={() => { setImage(null); }}>
+                        onPress={() => {
+                            // Caso a foto 1 seja removida e existir uma foto 2, a foto 2 passa para a primeira posição
+                            images[1] ? setImages([images[1]]) : setImages([]);
+                            console.log("Imagem deletada");
+                        }}>
                             <Image
                             source={require("../../../../assets/img/buttons/buttonDel.png")}
                             style={styles.buttonImage}/>
@@ -87,22 +89,30 @@ function Fotos({navigation}) {
                         }
                     </View>
 
+                    {/* Container da Foto 2 */}
                     <View style={styles.fotoContainer}>
-                        {image2 && <Image source={{ uri: image2 }} style={styles.foto} />}
+                        {/* Mostra a Foto 2 se existir */}
+                        {images[1] && <Image source={{ uri: images[1] }} style={styles.foto} />}
 
-                        {!image2 &&
+                        {/* Botão para adicionar a Foto 2 */}
+                        {!images[1] &&
                         <TouchableHighlight
                         style={styles.buttonAdd}
-                        onPress={pickImage2}>
+                        onPress={pickImage}>
                             <Image
                             source={require("../../../../assets/img/buttons/buttonAdd.png")}
                             style={styles.buttonImage}/>
                         </TouchableHighlight>}
 
-                        {image2 &&
+                        {/* Botão para remover a Foto 2 */}
+                        {images[1] &&
                         <TouchableHighlight
                         style={styles.buttonDel}
-                        onPress={() => { setImage2(null); }}>
+                        onPress={() => {
+                            // Caso a foto 2 seja removida, o array recebe apenas a primeira foto
+                            setImages([images[0]]);
+                            console.log("Imagem deletada");
+                        }}>
                             <Image
                             source={require("../../../../assets/img/buttons/buttonDel.png")}
                             style={styles.buttonImage}/>
@@ -110,11 +120,15 @@ function Fotos({navigation}) {
                         }
 
                     </View>
-
                 </View>
             </ScrollView>
+
+            {/* Error message, só aparece se o usuário tentar continuar sem foto */}
+            {errorMessage &&
+            <ErrorMessage message="Adicione pelo menos 1 foto para continuar"/>}
+
             <ContinuarButton
-                onPress={() => navigation.navigate('Sobre')}
+                onPress={handleImages}
             />
         </View>
     );
@@ -132,15 +146,17 @@ const styles = StyleSheet.create({
         marginTop: "25%",
     },
     fotoContainer: {
-        width: 150,
-        height: 240,
+        //width: 150,
+        width: '32%',
+        height: '65%',
+        //height: 240,
         marginRight: "5%",
         backgroundColor: "#3f3f46",
         borderRadius: 10,
     },
     foto: {
-        width: 150,
-        height: 240,
+        width: '100%',
+        height: '100%',
         borderRadius: 10,
     },
     buttonAdd: {
@@ -160,7 +176,7 @@ const styles = StyleSheet.create({
         borderRadius: 50,
     },
     buttonImage: {
-        width: 25,
-        height: 25,
+        width: "100%",
+        height: "100%",
     },
 });
