2b03cfe9ea7508050a47c350e68ee8e0d9aae0f0
Nycole Ribeiro <rpnyck@gmail.com>
2022-06-23 17:26:31 -0300

    Update Css.js

--- src/screens/Login/dadosEmpresa/Css.js
+++ src/screens/Login/dadosEmpresa/Css.js
@@ -1,13 +1,15 @@
 import { StyleSheet } from "react-native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 const css = StyleSheet.create({
     container: {
         flex: 1,
+        height: '100%',
         backgroundColor: '#18181b',
         textColor: '#fff',
     },
     h1: {
-        fontSize: 40,
+        fontSize: RFPercentage(5.3),
         color: '#fff',
         fontFamily: 'Inter_600SemiBold',
         marginTop: 80,
