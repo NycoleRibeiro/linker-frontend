6ec6404af68ddd6a38e8065c1822306f5a5fb8db
Nycole Ribeiro <rpnyck@gmail.com>
2022-07-02 16:55:46 -0300

    test: more enterprise data

--- assets/dadosTeste.js
+++ assets/dadosTeste.js
@@ -2,8 +2,11 @@ export const empresas = [
     {
         id: 1,
         nome: 'Google',
-        bio: 'Google é uma empresa de tecnologia do mundo.',
+        bio: 'Uma empresa multinacional de serviços online e software dos Estados Unidos.',
         imagens: ['https://play-lh.googleusercontent.com/RZ5luCUwc5QtJP9xDn-ZCwEutT160GVyoh5K1eu4YJ5fD7v4LP5ptVdgR9mz4Hnr7A', 'https://cdn.vox-cdn.com/thumbor/QIJJcaSeTulwA_UDheOfapQdq_0=/0x0:2040x1360/1200x800/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/70775925/acastro_180427_1777_0001.0.jpg'],
+        site: 'https://www.google.com/',
+        telefone: '+5553999999999',
+        localizacao: 'São Paulo, SP',
         vagas: [
             {
                 id: 1,
