2578fe62f5ea9edef10164d891f7e221d066ba74
Nycole Ribeiro <rpnyck@gmail.com>
2022-07-09 16:21:59 -0300

    feat: developer profile cv

--- src/screens/Desenvolvedor/Profile/Css.js
+++ src/screens/Desenvolvedor/Profile/Css.js
@@ -1,16 +1,23 @@
 import { StyleSheet } from "react-native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 const css = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#18181b",
-        alignItems: "center",
-        justifyContent: "center"
     },
     title: {
-        fontSize: 25,
-        fontWeight: "bold",
-        color: "#fff",
+        width: "100%",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        marginTop: 5,
+        marginBottom: 5,
+        fontSize: RFPercentage(2),
+        fontFamily: 'Inter_600SemiBold',
+        textTransform: 'uppercase',
+        textAlign: "center",
+        color: '#f4f4f5',
+        backgroundColor: '#1c1c20',
     },
 });
 
--- src/screens/Desenvolvedor/Profile/Profile.jsx
+++ src/screens/Desenvolvedor/Profile/Profile.jsx
@@ -1,13 +1,179 @@
 import React, {useState, useEffect} from 'react';
 import { View,
-    Text}
+        Text,
+        ScrollView,
+        TouchableHighlight,}
 from 'react-native';
-import { css } from './Css.js';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-export function Profile() {
+import { css } from './Css.js';
+import HeaderInfo from '../../../components/ProfileComponents/HeaderInfo.jsx';
+import AppHeader from '../../../components/AppHeader.jsx';
+import FloatButton from '../../../components/ProfileComponents/FloatButton.jsx';
+import ContactButtons from '../../../components/ProfileComponents/ContactButtons.jsx';
+import { RFPercentage } from "react-native-responsive-fontsize";
+
+import { desenvolvedores } from '../../../../assets/dadosTeste.js'
+
+export function Profile({navigation, route}) {
+
+    //dados para teste, será substituido por dados do banco
+    const dev = desenvolvedores[0];
+    if (route.params) {
+        const dev = route.params.dev;
+    }
+
     return (
         <View style={css.container}>
-            <Text style={css.title}>Profile</Text>
+
+            <AppHeader
+            headerType='image'/>
+
+            <HeaderInfo
+            profileImage={dev.imagens[0]}
+            profileName={dev.nome}
+            profileBio={dev.bio} />
+
+            <ScrollView
+            overScrollMode='never'
+            showsVerticalScrollIndicator
+            contentContainerStyle={{
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+            style={{
+                width: "100%",
+                height: "100%",
+                paddingHorizontal: 5}}
+            >
+                <ContactButtons
+                types={["whatsapp", "email", "linkedin", "github"]}
+                phone={dev.telefone}
+                email={dev.email}
+                linkedin={dev.linkedin}
+                github={dev.github}/>
+
+                {/* Dados Nascimento, Local e Formação */}
+                <View style={{
+                    width: "100%",
+                    marginLeft: 40,
+                }}>
+
+                    <View style={{
+                        width: '100%',
+                        flexDirection: 'row',
+                        marginTop:30,}}>
+                        <FontAwesome5 name="birthday-cake" size={20} color="#3f3f46" />
+                        <Text style={{color:'#a1a1aa', marginLeft:15}}>
+                            {dev.nascimento.mes} de {dev.nascimento.ano}
+                        </Text>
+                    </View>
+
+                    <View style={{
+                        width: '100%',
+                        flexDirection: 'row',
+                        marginTop:5,}}>
+                        <Ionicons name="md-location-sharp" size={20} color="#3f3f46" />
+                        <Text style={{color:'#a1a1aa', marginLeft:13}}>
+                            {dev.localizacao}
+                        </Text>
+                    </View>
+
+                    <View style={{
+                        width: '100%',
+                        flexDirection: 'row',
+                        marginTop:5,}}>
+                        <Ionicons name="school" size={20} color="#3f3f46" />
+                        <Text style={{color:'#a1a1aa', marginLeft:13}}>
+                            {dev.formacao}
+                        </Text>
+                    </View>
+                </View>
+
+                {dev.hardSkills.length > 0 &&
+                <>
+                <Text style={css.title}>Hard Skills</Text>
+                <View style={{
+                flexDirection: "row",
+                flexWrap: "wrap",
+                marginHorizontal: 10,
+                alignItems: "center",
+                justifyContent: "center",
+                }}>
+                    {dev.hardSkills.map((skill, index) => (
+                        <View key={index} style={{
+                        backgroundColor: "#27272a",
+                        marginRight: 8,
+                        marginBottom: 8,
+                        paddingHorizontal: 10,
+                        borderRadius: 10,
+                        }}>
+                            <Text style={{
+                            color: "#f4f4f5",
+                            fontSize: RFPercentage(1.7),
+                            fontFamily: 'Inter_400Regular',
+                            }}>
+                                {skill}
+                            </Text>
+                        </View>
+                    ))}
+                </View>
+                </>}
+
+                {dev.softSkills.length > 0 &&
+                <>
+                <Text style={css.title}>Soft Skills</Text>
+                <View style={{
+                    alignItems: "flex-start",
+                    justifyContent: "flex-start",
+                    alignSelf: "flex-start",
+                    marginHorizontal: 20,
+                }}>
+                    {dev.softSkills.map((skill, index) => (
+                        <Text
+                        key={index}
+                        style={{
+                            color: "#A1A1AA",
+                            fontSize: RFPercentage(1.8),
+                            fontFamily: 'Inter_400Regular'
+                        }}
+                        >
+                            • {skill}
+                        </Text>
+                    ))}
+                </View>
+                </>}
+
+                {dev.experiencia != '' &&
+                <>
+                <Text style={css.title}>Experiência</Text>
+                <Text style={{
+                    alignItems: "flex-start",
+                    justifyContent: "flex-start",
+                    alignSelf: "flex-start",
+                    marginHorizontal: 20,
+                    color: "#A1A1AA",
+                    fontSize: RFPercentage(1.8),
+                    fontFamily: 'Inter_400Regular'
+                }}>
+                    {dev.experiencia}
+                </Text>
+                </>}
+
+                {dev.certificacoes.length > 0 &&
+                <>
+                <Text style={css.title}>Certificações</Text>
+                </>}
+
+
+            </ScrollView>
+
+            {/* Botão de editar */}
+            <FloatButton
+            buttonType='edit'
+            onPress={() => navigation.navigate('EditProfile', {dev: dev})}/>
+
         </View>
     );
 }
