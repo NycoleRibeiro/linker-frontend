5ccb360b916efa2152858d10060a324218213bac
Nycole Ribeiro <rpnyck@gmail.com>
2022-07-15 16:34:01 -0300

    feat: create options bottom modal

---
+++ src/components/ProfileComponents/OptionsModal.jsx
@@ -0,0 +1,196 @@
+import React, { useState } from 'react';
+import { View,
+        Text,
+        TouchableHighlight,
+        Pressable,
+        Animated, }
+        from 'react-native';
+import { StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { RFPercentage } from "react-native-responsive-fontsize";
+
+export default function OptionsModal(props) {
+    const [bottom] = useState(new Animated.Value(-190));
+
+    Animated.timing(
+        bottom,
+        {
+            toValue: 0,
+            duration: 400,
+            useNativeDriver: false,
+        }
+    ).start();
+
+    return (
+        <Pressable
+        style={css.blur}
+        onPress={props.close}>
+
+            <Animated.View style={{
+                flexDirection: 'column',
+                position: 'absolute',
+                bottom: bottom,
+                width: '100%',
+                height: '30%',
+                alignItems: 'center',
+                backgroundColor: '#18181f',
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20,
+            }}>
+                <View style={css.header}>
+                    <MaterialCommunityIcons
+                    name="dots-grid"
+                    size={30}
+                    color="#52525b"
+                    style={{
+                        position: 'absolute',
+                        left: 10,
+                        top: 5,
+                    }}/>
+                    <Text style={css.title}>
+                        {props.title}
+                    </Text>
+                </View>
+
+
+                <View style={css.buttonsArea}>
+
+                    {/*BOTÃO EDITAR*/}
+                    {props.buttons.includes('editar') &&
+                        <View style={css.buttonArea}>
+                            <TouchableHighlight
+                            underlayColor="#fd2a7b"
+                            style={css.button}
+                            onPress={props.onPressEdit}>
+                                    <MaterialIcons
+                                    name="edit"
+                                    style={css.buttonIcon}/>
+                            </TouchableHighlight>
+                            <Text style={css.buttonLabel}>
+                                editar
+                            </Text>
+                        </View>
+                    }
+
+                    {/*BOTÃO EXCLUIR*/}
+                    {props.buttons.includes('excluir') &&
+                        <View style={css.buttonArea}>
+                            <TouchableHighlight
+                            underlayColor="#ff0000"
+                            style={css.buttonAttention}
+                            onPress={props.onPressDelete}>
+                                    <MaterialCommunityIcons
+                                    name="trash-can"
+                                    style={css.buttonIcon}/>
+                            </TouchableHighlight>
+                            <Text style={css.buttonLabel}>
+                                excluir
+                            </Text>
+                        </View>
+                    }
+
+                    {/*BOTÃO DETALHES*/}
+                    {props.buttons.includes('detalhes') &&
+                        <View style={css.buttonArea}>
+                            <TouchableHighlight
+                            underlayColor="#fd2a7b"
+                            style={css.button}
+                            onPress={props.onPressDetail}>
+                                    <MaterialCommunityIcons
+                                    name="text-long"
+                                    style={css.buttonIcon}/>
+                            </TouchableHighlight>
+                            <Text style={css.buttonLabel}>
+                                detalhes
+                            </Text>
+                        </View>
+                    }
+
+                    {/*BOTÃO ADICIONAR*/}
+                    {props.buttons.includes('adicionar') &&
+                        <View style={css.buttonArea}>
+                            <TouchableHighlight
+                            underlayColor="#fd2a7b"
+                            style={css.button}
+                            onPress={props.onPressAdd}>
+                                    <MaterialCommunityIcons
+                                    name="shape-square-rounded-plus"
+                                    style={css.buttonIcon}/>
+                            </TouchableHighlight>
+                            <Text style={css.buttonLabel}>
+                                adicionar
+                            </Text>
+                        </View>
+                    }
+
+                </View>
+            </Animated.View>
+        </Pressable>
+    )
+}
+
+const css = StyleSheet.create({
+    blur: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        zIndex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'center',
+        paddingVertical: 10,
+    },
+    title: {
+        fontSize: RFPercentage(2.3),
+        color: '#fff',
+        fontFamily: 'Inter_700Bold',
+        textTransform: 'uppercase',
+    },
+    buttonsArea: {
+        flex: 1,
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonArea: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginHorizontal: 10,
+    },
+    button: {
+        width: 80,
+        height: 80,
+        borderRadius: 100,
+        backgroundColor: '#FE4072',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    buttonAttention: {
+        width: 80,
+        height: 80,
+        borderRadius: 100,
+        backgroundColor: '#7c0000',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    buttonIcon: {
+        color: '#fff',
+        fontSize: 55,
+    },
+    buttonLabel: {
+        fontSize: RFPercentage(1.8),
+        color: '#fff',
+        fontFamily: 'Inter_400Regular',
+        textTransform: 'lowercase',
+        marginTop: 10,
+    },
+});
