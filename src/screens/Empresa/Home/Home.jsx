609434e32b666fdef65f092dc760db785d55605e
Nycole Ribeiro <rpnyck@gmail.com>
2022-07-28 17:01:53 -0300

    feat: enterprise home page

--- src/screens/Empresa/Home/Home.jsx
+++ src/screens/Empresa/Home/Home.jsx
@@ -4,10 +4,55 @@ import { View,
 from 'react-native';
 import { css } from './Css.js';
 
+import AppHeader from '../../../components/AppHeader'
+import HomeButtons from '../../../components/Button/HomeButtons'
+import DevView from '../../../components/ProfileComponents/DevView'
+
+// Dados teste... remover após implementar banco de dados
+import { empresas } from '../../../../assets/dadosTeste.js'
+import { desenvolvedores } from '../../../../assets/dadosTeste.js'
+
 export function Home() {
+    //dados para teste, será substituido por dados do banco
+    const empresa = empresas[0];
+    const [devsRow, setDevsRow] = useState([desenvolvedores[0], desenvolvedores[1], desenvolvedores[2]]);
+    const [currentDev, setCurrentDev] = useState(desenvolvedores[0])
+
+    const returnDev = () => {
+        // ao clicar no botão (return), retorna para a vaga anterior
+        let index = devsRow.indexOf(currentDev);
+        if (index > 0) {
+            setCurrentDev(devsRow[index - 1]);
+        }
+    }
+
+    const passDev = () => {
+        // ao clicar no botão (x) passa para a próxima vaga
+        let index = devsRow.indexOf(currentDev);
+        if (index < devsRow.length - 1) {
+            setCurrentDev(devsRow[index + 1]);
+        }
+    }
+
+    const okDev = () => {
+        // o teste da vaga vai ser enviado pra página de likes
+        // tela inicial passa para a proxima vaga
+        passDev()
+    }
+
     return (
         <View style={css.container}>
-            <Text style={css.title}>Home</Text>
+            <AppHeader
+            headerType='image'/>
+
+            <DevView
+            dev={currentDev}
+            />
+
+            <HomeButtons
+            onPressReturn={returnDev}
+            onPressOk={okDev}
+            onPressPass={passDev}/>
         </View>
     );
 }
