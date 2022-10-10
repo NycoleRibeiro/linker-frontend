51fdbfe6ec782d33a1ff071f87ffd49ad67aaf6c
Nycole Ribeiro <rpnyck@gmail.com>
2022-10-10 15:02:37 -0300

    style: Likes dev page

--- src/screens/Desenvolvedor/Likes/Css.js
+++ src/screens/Desenvolvedor/Likes/Css.js
@@ -1,17 +1,56 @@
 import { StyleSheet } from "react-native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 const css = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#18181b",
         alignItems: "center",
-        justifyContent: "center"
+    },
+    scroll: {
+        width: "100%",
+    },
+    likedVagas: {
+        width: "100%",
+        paddingHorizontal: 20,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
     },
     title: {
-        fontSize: 25,
-        fontWeight: "bold",
+        width: "100%",
+        paddingVertical: 20,
+        fontSize: RFPercentage(2),
+        fontFamily: 'Inter_400Regular',
         color: "#fff",
+        textAlign: "center",
     },
+    likedVaga: {
+        width: "100%",
+        height: "100%",
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "center",
+    },
+    vagaImage: {
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: -1,
+    },
+    likedVagaName: {
+        width: "100%",
+        height: 40,
+        textAlign: "center",
+        textAlignVertical: "center",
+        fontSize: RFPercentage(2),
+        fontFamily: 'Inter_400Regular',
+        color: "#fff",
+        backgroundColor: "rgba(24, 24, 27, 0.7)",
+    },
+
 });
 
 export {css};
