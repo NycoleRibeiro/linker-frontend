cb58cf9537ce438bf6a2dda0bdfa05427a776467
Nycole Ribeiro <rpnyck@gmail.com>
2022-12-12 14:03:33 -0300

    feat: Tela explorar do desenvolvedor

--- src/screens/Desenvolvedor/Explore/Css.js
+++ src/screens/Desenvolvedor/Explore/Css.js
@@ -1,16 +1,55 @@
 import { StyleSheet } from "react-native";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 const css = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#18181b",
         alignItems: "center",
-        justifyContent: "center"
+    },
+    scroll: {
+        width: "100%",
+    },
+    categorias: {
+        width: "100%",
+        paddingHorizontal: 20,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        //marginTop: 20,
     },
     title: {
-        fontSize: 25,
-        fontWeight: "bold",
+        width: "100%",
+        paddingVertical: 20,
+        fontSize: RFPercentage(2),
+        fontFamily: 'Inter_400Regular',
         color: "#fff",
+        textAlign: "center",
+    },
+    categoria: {
+        width: "100%",
+        height: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    categoriaImage: {
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: -1,
+    },
+    categoriaName: {
+        width: "100%",
+        height: 60,
+        textAlign: "center",
+        textAlignVertical: "center",
+        fontSize: RFPercentage(2),
+        fontFamily: 'Inter_400Regular',
+        color: "#fff",
+        backgroundColor: "rgba(24, 24, 27, 0.8)",
     },
 });
 
